de9d839fa824838431b72b63db12f7655f109ede
Priest4612 <prie4612@gmail.com>
2017-02-09 07:16:35 +0300

    Замечания устранены

--- js/form.js
+++ js/form.js
@@ -9,6 +9,7 @@ var formCancel = upload.querySelector('.upload-form-cancel');
 
 var filters = upload.querySelectorAll('.upload-filter-label');
 var imagePreview = upload.querySelector('.filter-image-preview');
+var prevFilter;
 
 var buttonDec = upload.querySelector('.upload-resize-controls-button-dec');
 var buttonInc = upload.querySelector('.upload-resize-controls-button-inc');
@@ -16,6 +17,7 @@ var resizeValue = upload.querySelector('.upload-resize-controls-value');
 var resizeMin = 25;
 var resizeMax = 100;
 var resizeStep = 25;
+var zoom;
 
 /**
  * Функция удаляет заданный класс в указанном элементе.
@@ -45,55 +47,106 @@ var checkContains = function (elem, cls) {
   return elem.classList.contains(cls);
 };
 
+var removePrevClass = function () {
+  if (prevFilter) {
+    removeClass(imagePreview, prevFilter);
+  }
+};
+
+/**
+* Функция добавляет % к числу num
+* @param {int} num число к которому необходимо добавить %
+* @return {string} num + '%'
+*/
+var addParcent = function (num) {
+  return num + '%';
+};
+
+/**
+ * Функция применяет к elem трансформацию
+ * @param {object} elem элемент DOM дерева
+ * @param {int} scale величина трансформации
+ */
+var setupZoom = function (elem, scale) {
+  elem.style.transform = 'scale(' + (scale / 100).toFixed(2) + ')';
+};
+
+/**
+ * Функция устанавливает стартовые значения
+ */
+var startSettings = function () {
+  zoom = resizeMax;
+  resizeValue.value = addParcent(resizeMax);
+  setupZoom(imagePreview, resizeMax);
+  removePrevClass();
+  addClass(buttonInc, invisible);
+  removeClass(buttonDec, invisible);
+};
+
+var isMax = function () {
+  if (zoom >= resizeMax) {
+    zoom = resizeMax;
+    addClass(buttonInc, invisible);
+  } else {
+    if (checkContains(buttonDec, invisible)) {
+      removeClass(buttonDec, invisible);
+    }
+  }
+};
+
+var isMin = function () {
+  if (zoom <= resizeMin) {
+    zoom = resizeMin;
+    addClass(buttonDec, invisible);
+  } else {
+    if (checkContains(buttonInc, invisible)) {
+      removeClass(buttonInc, invisible);
+    }
+  }
+};
+
+var calcZoom = function (action) {
+  if (action) {
+    zoom += resizeStep;
+    isMax();
+  } else {
+    zoom -= resizeStep;
+    isMin();
+  }
+  return zoom;
+};
+
 uploadFile.addEventListener('change', function () {
+  startSettings();
   removeClass(uploadOverlay, invisible);
   addClass(uploadSelectImage, invisible);
 });
 
 formCancel.addEventListener('click', function () {
   uploadFile.value = '';
+  startSettings();
   removeClass(uploadSelectImage, invisible);
   addClass(uploadOverlay, invisible);
 });
 
 for (var i = 0; i < filters.length; i++) {
   filters[i].addEventListener('click', function (e) {
+    removePrevClass();
     var currentFilter = 'filter-' + document.querySelector('#' + e.currentTarget.htmlFor).value;
-    for (var j = imagePreview.classList.length - 1; j !== 0; j--) {
-      if (imagePreview.classList[j] !== 'filter-image-preview') {
-        removeClass(imagePreview, imagePreview.classList[j]);
-      }
-    }
+
     addClass(imagePreview, currentFilter);
+    prevFilter = currentFilter;
   });
 }
 
 buttonDec.addEventListener('click', function () {
-  var rValue = +resizeValue.value.slice(0, -1);
-  rValue -= resizeStep;
-  if (rValue <= resizeMin) {
-    addClass(buttonDec, invisible);
-    rValue = resizeMin;
-  }
-  if (checkContains(buttonInc, invisible)) {
-    removeClass(buttonInc, invisible);
-  }
-  var scale = ['scale(', (rValue / 100).toFixed(2), ')'].join('');
-  imagePreview.style.transform = scale;
-  resizeValue.value = [rValue, '%'].join('');
+  var rValue = calcZoom(false);
+  setupZoom(imagePreview, rValue);
+  resizeValue.value = addParcent(rValue);
 });
 
 buttonInc.addEventListener('click', function () {
-  var rValue = +resizeValue.value.slice(0, -1);
-  rValue += resizeStep;
-  if (rValue >= resizeMax) {
-    addClass(buttonInc, invisible);
-    rValue = resizeMax;
-  }
-  if (checkContains(buttonDec, invisible)) {
-    removeClass(buttonDec, invisible);
-  }
-  var scale = ['scale(', (rValue / 100).toFixed(2), ')'].join('');
-  imagePreview.style.transform = scale;
-  resizeValue.value = [rValue, '%'].join('');
+  var rValue = calcZoom(true);
+  setupZoom(imagePreview, rValue);
+  resizeValue.value = addParcent(rValue);
 });
