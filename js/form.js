262b22095b3de34d784d5a2905d4d70585ec3539
Priest4612 <prie4612@gmail.com>
2017-02-25 05:42:08 +0300

    Установлен начальный масштаб изображения

--- js/form.js
+++ js/form.js
@@ -1,5 +1,6 @@
 'use strict';
 var cssClassInvisible = 'invisible';
+var startScale = 100;
 
 var uploadOverlay = document.querySelector('.upload-overlay');
 var uploadImage = document.querySelector('#upload-select-image');
@@ -14,16 +15,19 @@ var resizeValue = document.querySelector('.upload-resize-controls-value');
 var openFormCropImage = function () {
   uploadImage.classList.add(cssClassInvisible);
   uploadOverlay.classList.remove(cssClassInvisible);
+  window.initializeScale.scale = startScale;
+  window.initializeFilters.removePrevFilter(imagePreview, window.initializeFilters.prevFilter);
+  window.initializeScale.setupScale(imagePreview, window.initializeScale.scale);
+  window.initializeScale.setResizeControlsValue(resizeValue, window.initializeScale.scale);
 };
 var closeFormCropImage = function () {
   uploadImage.classList.remove(cssClassInvisible);
   uploadOverlay.classList.add(cssClassInvisible);
   uploadFile.value = '';
-  window.initializeFilters.removePrevFilter(imagePreview, window.initializeFilters.prevFilter);
 };
 
 uploadFile.addEventListener('change', openFormCropImage);
 uploadFormCancel.addEventListener('click', closeFormCropImage);
 window.initializeFilters.setFilter(filters, imagePreview);
-window.initializeScaleImage.resizeDecScale(buttonDec, imagePreview, resizeValue);
-window.initializeScaleImage.resizeIncScale(buttonInc, imagePreview, resizeValue);
+window.initializeScale.resizeDecScaleImage(buttonDec, imagePreview, resizeValue);
+window.initializeScale.resizeIncScaleImage(buttonInc, imagePreview, resizeValue);
